395647c7c124f2cca2a5e75f2d30fa037d1cd507
alex <beachsidewebdesignllc@gmail.com>
2023-03-09 18:53:38 -0500

    add event listener to button to check passwords

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // alert("hello");
+
+let pwd = document.querySelector("#pwd");
+
+let pwdConfirm = document.querySelector("#pwd-confirm");
+
+let button = document.querySelector("button");
+
+button.addEventListener("click", checkPwd);
+
+function checkPwd() {
+  if (pwd.value === pwdConfirm.value) return;
+
+  alert("Passwords do not match");
+}
